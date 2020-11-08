4ef5caada81f07e3f6973f3867bb54531229ddb6
ps-astangl <astangl@leaporbit.com>
2020-11-08 13:38:22 -0500

    Changes to oauth

--- src/app/launch/launch.component.ts
+++ src/app/launch/launch.component.ts
@@ -11,32 +11,32 @@ import Client from 'fhirclient/lib/Client';
 })
 export class LaunchComponent implements OnInit {
   public patientData = null;
-  constructor(@Inject(DOCUMENT) private document: Document) {
-  }
+  // constructor(@Inject(DOCUMENT) private document: Document) {
+  // }
 
   ngOnInit(): void {
       // The issuer goes here. I am using smartHealthIT for this application
       // tslint:disable-next-line:max-line-length
       // https://launch.smarthealthit.org/?auth_error=&fhir_version_1=r4&fhir_version_2=r4&iss=&launch_ehr=1&launch_url=http%3Alocalhost%3A4200%2Flaunch&patient=&prov_skip_auth=1&provider=&pt_skip_auth=1&public_key=&sb=&sde=&sim_ehr=0&token_lifetime=15&user_pt=
-    const issuer = 'https://launch.smarthealthit.org/v/r4/fhir';
+    // SmartHealth Server: https://launch.smarthealthit.org/v/r4/fhir
+    const issuer = 'https://apporchard.epic.com/interconnect-aocurprd-oauth/api/FHIR/R4';
     const options = {
       // The client Id goes here
-      clientId: 'client_app_id_goes_here',
-      // The scope necessary for the operation of the application is declared -- For Epic I believe you only need patient
-      scope: [
-        'launch',          // Get current context of the EHR
-        'launch patient', // Context for patient
-        'openid fhirUser',  // Get current user
-      ].join(' '),
+      clientId: 'client_id_for_epic_here',
+      // The scope necessary for the operation of the application is declared -- For Epic the required scopes are below
+      scope: 'launch launch/patient openid profile online_access',
       redirectUri: 'http://localhost:4200/launch',
-      iss: issuer
+      iss: issuer,
+      completeInTarget: true
     };
 
     /*
      This gets a little tricky since I am doing the retrieval of data with init instead of authorize which would require me to use the
      code and get the bearer token When done this way you have to get all the FHIR resources you want at once which may not be what you
      want but I hope this helps get the general idea across.
+      const auth = smart.oauth2.authorize(options); Another method is to use authorize.
     */
+
     smart.oauth2.init(options).then(async (x) => {
       // @ts-ignore
       let patientObject: fhirclient['FHIR.Patient'];
